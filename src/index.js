0bd341e3a8b51e5b5764ebb981a4d79633a9a46a
Robert Goon <rob.goon@gmail.com>
2018-04-28 02:31:17 -0400

    switched to weapon plugin

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function preload () {
     game.load.image('sky', 'assets/sky.png');
     game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64, 23);
     game.load.spritesheet('player', 'assets/dude.png', 32, 48);
-    game.load.spritesheet('weapons', 'assets/weapons.gif', 120, 60);
+    game.load.spritesheet('weapons', 'assets/weapons.gif', 105, 67);
 
 }
 
@@ -31,6 +31,7 @@ let explosions;
 let player;
 let facing = 'right'
 
+let weaponSprite;
 let weapon;
 
 let bullets;
@@ -51,24 +52,26 @@ let altLeftKey;
 let altRightKey;
 let jumpKey;
 
+const GRAVITY = 400;
 const ROTATE_90_DEG_IN_RAD = Math.PI/2;
 
 function create () {
 
-    //  Resize our game world to be a 1600 x 800 square
+    /* Resize our game world to be a 1600 x 800 square */
     game.world.setBounds(-800, -725, 1600, 800);
-    game.physics.arcade.gravity.y = 400;
+    game.physics.arcade.gravity.y = GRAVITY;
 
-    //  Our tiled scrolling background
+    /* Our tiled scrolling background */
     land = game.add.tileSprite(0, 0, 800, 600, 'sky');
     land.fixedToCamera = true;
 
-    // Player
+    /* Player */
     player = game.add.sprite(0, 0, 'player');
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.collideWorldBounds = true;
+    player.scale.setMagnitude(2);
     player.body.setSize(26, 35, 3, 13); // Hitbox/Collision model
 
     player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -90,8 +93,35 @@ function create () {
     turret = game.add.sprite(0, 0, 'tank', 'turret');
     turret.anchor.setTo(0.3, 0.5);
 
-    weapon = game.add.sprite(0, 0, 'weapons', 1);
-    weapon.anchor.setTo(0.3, 0.5);
+    /* Default weapon */
+    weaponSprite = game.add.sprite(0, 0, 'weapons', 0);
+    weaponSprite.anchor.setTo(0.3, 0.1);
+    weaponSprite.scale.setMagnitude(.8);
+    game.physics.enable(weaponSprite, Phaser.Physics.ARCADE);
+    weaponSprite.body.collideWorldBounds = true;
+
+    /* Bullets */
+
+    //  Creates 30 bullets, using the 'bullet' graphic
+    weapon = game.add.weapon(30, 'bullet');
+    weapon.bullets.scale.setMagnitude(2);
+    weapon.bulletGravity.y = -GRAVITY;
+
+    //  The bullet will be automatically killed when it leaves the world bounds
+    // weapon.bulletKillType = Phaser.Weapon.KILL_WEAPON_BOUNDS;
+
+    // Rotate bullets
+    weapon.bulletAngleOffset = 90;
+
+    //  The speed at which the bullet is fired
+    weapon.bulletSpeed = 400;
+
+    //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+    weapon.fireRate = FIRERATE_RIFLE;
+
+    //  Tell the Weapon to track the 'weaponSprite' Sprite
+    //  But the 'true' argument tells the weapon to track sprite rotation
+    weapon.trackSprite(weaponSprite, 0, 0, true);
 
     //  The enemies bullet group
     enemyBullets = game.add.group();
@@ -239,14 +269,22 @@ function update () {
     shadow.y = tank.y;
     shadow.rotation = tank.rotation;
 
-    turret.x = player.x;
-    turret.y = player.y;
+    turret.x = tank.x;
+    turret.y = tank.y;
 
     turret.rotation = game.physics.arcade.angleToPointer(turret);
+
+    weaponSprite.x = player.x;
+    weaponSprite.y = player.y;
+
+    weaponSprite.rotation = game.physics.arcade.angleToPointer(weaponSprite);
+
+
 
     if (game.input.activePointer.isDown)
     {
         //  Boom!
+        weapon.fire();
         fire();
     }
 
@@ -291,88 +329,7 @@ function fire () {
 
 function render () {
 
-    game.debug.text('Active Bullets: ' + bullets.countLiving() + ' / ' + bullets.length, 32, 64);
-    game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
-    // game.debug.bodyInfo(tank, 32, 96);
-    // game.debug.body(tank);
-    game.debug.bodyInfo(player, 32, 96);
-    game.debug.body(player);
+    game.debug.bodyInfo(weaponSprite, 32, 96);
+    // game.debug.body(player); // Hitbox/Collision model
+    // weapon.debug()
 }
-
-const EnemyTank = function (index, game, player, bullets) {
-
-  var x = game.world.randomX;
-  var y = game.world.randomY;
-
-  this.game = game;
-  this.health = 3;
-  this.player = player;
-  this.bullets = bullets;
-  this.fireRate = 1000;
-  this.nextFire = 0;
-  this.alive = true;
-
-  this.shadow = game.add.sprite(x, y, 'enemy', 'shadow');
-  this.tank = game.add.sprite(x, y, 'enemy', 'tank1');
-  this.turret = game.add.sprite(x, y, 'enemy', 'turret');
-
-  this.shadow.anchor.set(0.5);
-  this.tank.anchor.set(0.5);
-  this.turret.anchor.set(0.3, 0.5);
-
-  this.tank.name = index.toString();
-  game.physics.enable(this.tank, Phaser.Physics.ARCADE);
-  this.tank.body.immovable = false;
-  this.tank.body.collideWorldBounds = true;
-  this.tank.body.bounce.setTo(1, 1);
-
-  this.tank.angle = game.rnd.angle();
-
-  game.physics.arcade.velocityFromRotation(this.tank.rotation, 100, this.tank.body.velocity);
-
-};
-
-EnemyTank.prototype.damage = function() {
-
-  this.health -= 1;
-
-  if (this.health <= 0)
-  {
-      this.alive = false;
-
-      this.shadow.kill();
-      this.tank.kill();
-      this.turret.kill();
-
-      return true;
-  }
-
-  return false;
-
-}
-
-EnemyTank.prototype.update = function() {
-
-  this.shadow.x = this.tank.x;
-  this.shadow.y = this.tank.y;
-  this.shadow.rotation = this.tank.rotation;
-
-  this.turret.x = this.tank.x;
-  this.turret.y = this.tank.y;
-  this.turret.rotation = this.game.physics.arcade.angleBetween(this.tank, this.player);
-
-  if (this.game.physics.arcade.distanceBetween(this.tank, this.player) < 300)
-  {
-      if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
-      {
-          this.nextFire = this.game.time.now + this.fireRate;
-
-          var bullet = this.bullets.getFirstDead();
-
-          bullet.reset(this.turret.x, this.turret.y);
-
-          bullet.rotation = this.game.physics.arcade.moveToObject(bullet, this.player, 500);
-      }
-  }
-
-};
