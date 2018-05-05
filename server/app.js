1448b76df6146dd20e7fe880c02a0f48d3b90159
Robert Goon <rob.goon@gmail.com>
2018-05-05 02:32:03 -0400

    added server

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 8080
+
+// Logging middleware
+app.use(morgan('dev'))
+
+// Body parsing middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// Static middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// If you want to add routes, they should go here!
+
+// For all GET requests that aren't to an API route,
+// we will send the index.html!
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'))
+})
+
+// Handle 404s
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error handling endware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message || 'Internal server error')
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: 'babel-loader'
+                }
+            }
+        ]
     }
 }
+
